36d3bb8ef2ea303a0c60b955b79cb4b761e0abb2
Andrej Vujnic <147924315+Vujnic@users.noreply.github.com>
2024-11-10 13:56:25 +0100

    Add files via upload

---
+++ KomponenteProjekatPrvi/spec/build.gradle.kts
@@ -0,0 +1,36 @@
+plugins {
+    kotlin("jvm")
+    `java-library`
+    `maven-publish`
+}
+
+group = "org.example"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation("org.jetbrains.kotlin:kotlin-test")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("mavenJava") {
+            from(components["java"]) // If you're using the 'java' or 'kotlin' plugin
+
+            groupId = "org.example"
+            artifactId = "spec"
+            version = "1.0.0"
+        }
+    }
+}
+
+kotlin {
+    jvmToolchain(21)
+}
